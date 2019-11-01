8a1027e5d87d6c515f8b07cf9e677f9656a31b35
NISHI, Takao <zophos@ni.aist.go.jp>
2019-11-01 09:41:31 +0900

    separate Calendar.prototype.set_cell_content

---
+++ app/static/calendar_setcell.js
@@ -0,0 +1,33 @@
+Calendar.prototype.set_cell_content=function(date,body)
+{
+    var el=this.cell(date);
+    if(!el)
+	return;
+    el.classList.remove('open');
+    el.classList.remove('closed');
+    el.classList.remove('canceld');
+    el.removeAttribute('data-you');
+
+    var td=el.getElementsByTagName('td')[0]
+    if(!td)
+	return;
+
+    if('all' in body){
+	td.innerText=parseInt(body['all'],10);
+	if(body['all']>=4)
+	    td.classList.add('go');
+    }
+    else{
+	td.innerText='';
+	return;
+    }
+
+    if('status' in body)
+	el.classList.add(body['status']);
+
+    if('you' in body && body['you']>0){
+	td.classList.add('you');
+	el.setAttribute('data-you',body['you']);
+    }
+
+}
